b13890b05aac8b92066afa67cd88a33df6544838
Enzo sch <enzo.schneider318@gmail.com>
2024-11-18 14:51:46 +0100

    14:51

---
+++ script.js
@@ -0,0 +1,6 @@
+//script AP2
+<script>
+document.getElementById('toggler').addEventListener('click', function () {
+                document.querySelector('.menu').classList.toggle('active');
+});
+</script>
